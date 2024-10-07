71e843227a36f0a60f956a2bbe9d4d6e2ab85620
blusque <kokutouaikata@outlook.com>
2024-10-08 01:37:40 +0800

    trivial commit

---
+++ Source/Aura/Private/Character/AuraCharacterBase.cpp
@@ -0,0 +1,20 @@
+// Copyright Blusque Mei
+
+
+#include "Character/AuraCharacterBase.h"
+
+AAuraCharacterBase::AAuraCharacterBase()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Weapon = CreateDefaultSubobject<USkeletalMeshComponent>(FName("Weapon"));
+	Weapon->SetupAttachment(GetMesh(), FName("WeaponHandSocket"));
+	Weapon->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+}
+
+void AAuraCharacterBase::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
---
+++ Source/Aura/Public/Character/AuraCharacterBase.h
@@ -0,0 +1,22 @@
+// Copyright Blusque Mei
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AuraCharacterBase.generated.h"
+
+UCLASS(Abstract)
+class AURA_API AAuraCharacterBase : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	AAuraCharacterBase();
+
+protected:
+	virtual void BeginPlay() override;
+	
+	UPROPERTY(VisibleAnywhere, Category="Combat")
+	TObjectPtr<USkeletalMeshComponent> Weapon;
+};
